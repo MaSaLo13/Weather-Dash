b47462a1e7391520b1b460395dd3c10e492cd47a
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-06-17 00:01:24 -0700

    5 day forecast being displayed

--- assets/script.js
+++ assets/script.js
@@ -46,7 +46,7 @@ function drawWeather(data) {
 	//document.getElementById('description').innerHTML = data.weather[0].description;
 	document.getElementById('location').innerHTML = data.name;
   document.getElementById('temp').innerHTML = 'Temp: ' + data.main.temp + '&deg;F';
-  document.getElementById('wind').innerHTML = 'Wind: ' + data.wind.speed + 'MPH';
+  document.getElementById('wind').innerHTML = 'Wind: ' + data.wind.speed + ' MPH';
   document.getElementById('humid').innerHTML = 'Humidity: ' + data.main.humidity + '%';
 };
 
@@ -91,8 +91,27 @@ function weatherQuery2(fiveWeath) {
 
 function fiveWeather(data) {
 	document.getElementById('date1').innerHTML = data.list[1].clouds.dt_txt;
-	//document.getElementById('location').innerHTML = data.name;
-  //document.getElementById('temp1').innerHTML = 'Temp: ' + data.main.temp + '&deg;F';
-  //document.getElementById('wind').innerHTML = 'Wind: ' + data.wind.speed + 'MPH';
-  //document.getElementById('humid').innerHTML = 'Humidity: ' + data.main.humidity + '%';
+  document.getElementById('temp1').innerHTML = 'Temp: ' + data.list[1].main.temp + '&deg;F';
+  document.getElementById('wind1').innerHTML = 'Wind: ' + data.list[1].wind.speed + ' MPH';
+  document.getElementById('humid1').innerHTML = 'Humidity: ' + data.list[1].main.humidity + '%';
+  // day 2
+  document.getElementById('date2').innerHTML = data.list[9].clouds.dt_txt;
+  document.getElementById('temp2').innerHTML = 'Temp: ' + data.list[9].main.temp + '&deg;F';
+  document.getElementById('wind2').innerHTML = 'Wind: ' + data.list[9].wind.speed + ' MPH';
+  document.getElementById('humid2').innerHTML = 'Humidity: ' + data.list[9].main.humidity + '%';
+  // day 3
+  document.getElementById('date3').innerHTML = data.list[17].clouds.dt_txt;
+  document.getElementById('temp3').innerHTML = 'Temp: ' + data.list[17].main.temp + '&deg;F';
+  document.getElementById('wind3').innerHTML = 'Wind: ' + data.list[17].wind.speed + ' MPH';
+  document.getElementById('humid3').innerHTML = 'Humidity: ' + data.list[17].main.humidity + '%';
+  //day 4
+  document.getElementById('date4').innerHTML = data.list[25].clouds.dt_txt;
+  document.getElementById('temp4').innerHTML = 'Temp: ' + data.list[25].main.temp + '&deg;F';
+  document.getElementById('wind4').innerHTML = 'Wind: ' + data.list[25].wind.speed + ' MPH';
+  document.getElementById('humid4').innerHTML = 'Humidity: ' + data.list[25].main.humidity + '%';
+  // day 5
+  document.getElementById('date5').innerHTML = data.list[33].clouds.dt_txt;
+  document.getElementById('temp5').innerHTML = 'Temp: ' + data.list[33].main.temp + '&deg;F';
+  document.getElementById('wind5').innerHTML = 'Wind: ' + data.list[33].wind.speed + ' MPH';
+  document.getElementById('humid5').innerHTML = 'Humidity: ' + data.list[33].main.humidity + '%';
 };
