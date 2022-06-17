c2d141fe041713885894af44ab575f1c39f49171
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-06-16 23:44:28 -0700

    5 day forecast API

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Weather-Dash
+
+## Description
+* Created a weather dashboard 
+    - When you type a city in the search bar, what will pop is the weather of the day as well as a 5 day forecast
+    - Within the weather for the day there is
+        - Temp, wind, humidity and UV indext
+    - 5 day forecast
+        - Temp, wind and humidity
+    - You are also able to view recent searches 
+
+# Screenshot
+![Screenshot of Day-Scheduler]
+
+
+repolink: 
+https://github.com/MaSaLo13/Weather-Dash
+
+deployed site:
+https://masalo13.github.io/Weather-Dash/
--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,6 @@ var searchBtn = document.querySelector('.search-btn');
 var apiKey = "d643534191603c995f211a0eaffc23c6";
 //var city;
 
-//var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-
-/*const weathApi = {
-  method: 'GET',
-  headers: {
-      'weathAPI-Key': 'd643534191603c995f211a0eaffc23c6',
-      'weath-Host': '/https://api.openweathermap.org/data/2.5/weather?q=' + searchBarInput + '&appid=' + apiKey,
-  }
-};*/
 
 //click event to built url??
 function searchClick(event) {
@@ -21,27 +12,13 @@ function searchClick(event) {
     console.log(city)
     // build first url request
   var weathUrl = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
-
+  var fiveWeath = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial&exclude=current,minutely,hourly,alerts";
  
-    // Call fetch functions
-    weatherQuery(weathUrl)
-   // recipeQuery(recipeString);
-    //nutritionQuery(nutritionString);
-    //console.log(location)
-    //window.location.href = 'results-page.html';
-    // return variables to use in fetch functions
-    //return nutritionString, recipeString;
-    //return weathUrl;
+    weatherQuery(weathUrl);
+    weatherQuery2(fiveWeath);
 };
 
-/*const weathUrl = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-fetch(weathUrl, {
-  method: "GET",
-  headers: {
-    "X-Auth-Token": "d643534191603c995f211a0eaffc23c6"
-  }
-})
-*/
+
 // shows results? 
 function weatherQuery(weathUrl) {
   //fetch with built URL to return recipe info
@@ -60,40 +37,19 @@ function weatherQuery(weathUrl) {
       .then(function (data) {
         drawWeather(data);
         console.log(data)
-          /*var resultArray = data;
-          displayResult(resultArray);
-          return resultArray;*/
       })
-      window.onload = function() {
-        weatherBalloon( 6167865 );
-}};
+};
 
 
 // help display?
-function drawWeather( d ) {
-	//var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-	//var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
-	
-	document.getElementById('description').innerHTML = d.weather[0].description;
-	//document.getElementById('temp').innerHTML = farenheit + '&deg;';
-	document.getElementById('location').innerHTML = d.name;
-  document.getElementById('temp').innerHTML = d.main.temp;
+function drawWeather(data) {
+	//document.getElementById('description').innerHTML = data.weather[0].description;
+	document.getElementById('location').innerHTML = data.name;
+  document.getElementById('temp').innerHTML = 'Temp: ' + data.main.temp + '&deg;F';
+  document.getElementById('wind').innerHTML = 'Wind: ' + data.wind.speed + 'MPH';
+  document.getElementById('humid').innerHTML = 'Humidity: ' + data.main.humidity + '%';
 };
 
-//need
-/*function weatherBalloon( city ) {
-	//var key = '{yourkey}';
-	fetch('https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?id=' + city + '&appid=' + apiKey + "&units=imperial")  
-	.then(function(resp) { return resp.json() }) // Convert data to json
-	.then(function(data) {
-		drawWeather(data); // Call drawWeather
-	})
-	.catch(function() {
-		// catch any errors
-	});
-};*/
-/*for (var i = 0; i < data.length; i++) {
-  console.log(data[i].name);*/
 
 searchBtn.addEventListener('click', searchClick);
 
@@ -109,3 +65,34 @@ city.addEventListener('keydown', (event) => {
 
 // 5 days 
 // "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial"*/
+
+
+function weatherQuery2(fiveWeath) {
+  //fetch with built URL to return recipe info
+  fetch(fiveWeath, {
+      method: 'GET',
+      headers: {
+           'apiKey': "d643534191603c995f211a0eaffc23c6",
+           'weather-host': "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial&exclude=current,minutely,hourly,alerts" ,
+  
+      }
+  })
+
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (data) {
+        fiveWeather(data);
+        console.log(data);
+      })
+      /*window.onload = function() {
+        weatherBalloon( 6167865 );*/
+};
+
+function fiveWeather(data) {
+	document.getElementById('date1').innerHTML = data.list[1].clouds.dt_txt;
+	//document.getElementById('location').innerHTML = data.name;
+  //document.getElementById('temp1').innerHTML = 'Temp: ' + data.main.temp + '&deg;F';
+  //document.getElementById('wind').innerHTML = 'Wind: ' + data.wind.speed + 'MPH';
+  //document.getElementById('humid').innerHTML = 'Humidity: ' + data.main.humidity + '%';
+};
